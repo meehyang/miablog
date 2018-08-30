f6755b79623323d7e4ff816d59640b7624aea431
먕 <>
2018-08-30 19:52:08 +0900

    BoardBO 테스트용 클래스 파일 생성

---
+++ src/test/java/com/mia/miablog/BoardBOTest.java
@@ -0,0 +1,56 @@
+package com.mia.miablog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.mia.miablog.bo.BoardBO;
+import com.mia.miablog.vo.BoardVO;
+
+//junit초기화 설
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+
+public class BoardBOTest {
+	@Autowired
+	private BoardBO boardBO;
+	
+	@Test
+	public void insert() {
+		BoardVO boardVO = new BoardVO();
+		
+		boardVO.setTitle("boardBO test title");
+		boardVO.setContent("메롱메롱메롱멜ㅇ");
+		boardVO.setUser_idx(7);
+		
+		boardBO.insert(boardVO);
+	}
+	
+	@Test
+	public void update() {
+		BoardVO boardVO = new BoardVO();
+		
+		boardVO.setTitle("BO 테스트 확인용 타이틀 update");
+		boardVO.setIdx(4);
+		
+		boardBO.update(boardVO);
+	}
+	
+	@Test
+	public void delete() {
+		BoardVO boardVO = new BoardVO();
+		
+		boardVO.setIdx(5);
+		
+		boardBO.delete(boardVO);
+	}
+	
+	@Test
+	public void selectList() {
+		boardBO.selectList();
+	}
+}
