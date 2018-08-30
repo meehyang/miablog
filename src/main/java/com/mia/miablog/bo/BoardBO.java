8d03c5234ae102c2c909951f6a9d6393600bf61e
먕 <>
2018-08-30 19:51:51 +0900

    BoardBO 생성

---
+++ src/main/java/com/mia/miablog/bo/BoardBO.java
@@ -0,0 +1,30 @@
+package com.mia.miablog.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mia.miablog.dao.BoardDAO;
+import com.mia.miablog.vo.BoardVO;
+
+public class BoardBO {
+	@Autowired
+	protected BoardDAO boardDAO;
+	
+	public void insert(BoardVO boardVO) {
+		boardDAO.insert(boardVO);
+	}
+	
+	public void update(BoardVO boardVO) {
+		boardDAO.update(boardVO);
+	}
+	
+	public void delete(BoardVO boardVO) {
+		boardDAO.delete(boardVO);
+	}
+	
+	public List<BoardVO> selectList() {
+		return boardDAO.selectList();
+	}
+}
+
