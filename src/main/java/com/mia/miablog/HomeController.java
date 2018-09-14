9dfc015c18b68c5504b843143db50f1c4f255dfc
먕 <>
2018-09-14 16:32:50 +0900

    메인 페이지, 메인의 상세 페이지를 위한 HomeController 수정

--- src/main/java/com/mia/miablog/HomeController.java
+++ src/main/java/com/mia/miablog/HomeController.java
@@ -2,20 +2,32 @@ package com.mia.miablog;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.mia.miablog.dao.AttachfileDAO;
+import com.mia.miablog.dao.BoardDAO;
+import com.mia.miablog.vo.AttachfileVO;
+import com.mia.miablog.vo.BoardVO;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
+	@Autowired
+	private BoardDAO boardDAO = new BoardDAO();
+	@Autowired
+	private AttachfileDAO attachfileDAO = new AttachfileDAO();
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -24,16 +36,27 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		List<BoardVO> boardList = boardDAO.selectList();
+		model.addAttribute("boardList", boardList);
 		
 		return "main/index";
+	}
+	
+	@RequestMapping(value="/view", method = RequestMethod.GET)
+	public String view(Locale locale, Model model, @RequestParam("idx") int idx) {
+		BoardVO board = boardDAO.select(idx);
+		AttachfileVO attachfile = attachfileDAO.select(idx);
+		//첨부파일이 존재하는 경우에만 넘겨준다 
+		if(attachfile!=null) {
+			model.addAttribute("attachView", attachfile);
+		}
+		//이전글 다음글 가져오기
+		BoardVO boardNum = boardDAO.selectNum(idx);
+		
+		model.addAttribute("boardView", board);
+		model.addAttribute("boardNum", boardNum);
+		
+		return "main/view";
 	}
 	
 }
